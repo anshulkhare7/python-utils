ba29c25c77cafc4b109f7e599df880544ca947f0
Anshul <anshulkhare7@gmail.com>
2021-03-10 13:58:26 +0530

    joinVideo added

--- audio2video.py
+++ audio2video.py
@@ -1,10 +1,37 @@
 from moviepy.editor import *
+from os import listdir
+from os.path import isfile, join
 
-# Import the audio(Insert to location of your audio instead of audioClip.mp3)
-audio = AudioFileClip(r"C:\Users\91998\Documents\abc.mp3")
-# Import the Image and set its duration same as the audio (Insert the location of your photo instead of photo.jpg)
-clip = ImageClip(r"C:\Users\91998\Documents\abc.jpg").set_duration(audio.duration)
-# Set the audio of the clip
-clip = clip.set_audio(audio)
-# Export the clip
-clip.write_videofile(r"C:\Users\91998\Documents\abc.mp4", fps=0.1)
+def convert(inDir, imagePath, outDir):
+
+    onlyfiles = [f for f in listdir(inDir) if isfile(join(inDir, f))]
+
+    for mp3File in onlyfiles:
+        if(mp3File.endswith(".mp3")):
+            filePath = inDir + mp3File
+            audio = AudioFileClip(filePath)
+            clip = ImageClip(imagePath).set_duration(audio.duration)        
+            fileName = outDir + os.path.splitext(mp3File)[0] + '.mp4'
+            clip = clip.set_audio(audio)
+            clip.write_videofile(fileName, fps=0.1)
+
+def joinVideo(inputdirectory):
+
+    onlyfiles = [f for f in listdir(inputdirectory) if isfile(join(inputdirectory, f))]
+
+    clips = []
+    for videofile in onlyfiles:
+        if(videofile.endswith(".mp4")):
+            filepath = inputdirectory + videofile
+            clip = VideoFileClip(filepath)
+            clips.append(clip)
+    
+    final_clip = concatenate_videoclips(clips)
+    final_clip.write_videofile(inputdirectory+'joined-video.mp4')
+
+if __name__=="__main__":    
+    inDir = r"C:\Users\91998\Documents\\"
+    imagePath = r"C:\Users\91998\Documents\myimage.jpg"
+    outDir = r"C:\Users\91998\Documents\\"
+    convert(inDir, imagePath, outDir)
+    joinVideo(outDir)
